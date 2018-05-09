1facd3944320915824ef02887a6065e88fb46084
rox65 <34298121+rox65@users.noreply.github.com>
2018-05-09 18:35:16 +0800

    For the Client-Side

---
+++ Client-Side/view.php
@@ -0,0 +1,18 @@
+<h1>Notification</h1>
+<?php
+    include("functions.php");
+
+$id= $_GET['id'];
+
+$query="UPDATE `notifications` SET `status` = 'read' WHERE `notifications`.`id` = $id;";
+performQuery($query);
+
+$query="SELECT * from notifications where id = '$id';";
+if(count(fetchAll($query)) >0){
+    foreach(fetchAll($query) as $i){
+        echo $i['message'];
+    }
+}
+?>
+<br>
+<a href="index.php">Back</a>
