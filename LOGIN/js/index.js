438354395022760c24abb030af169ba96aca7121
tedtower <34298083+tedtower@users.noreply.github.com>
2018-05-10 18:45:40 +0800

    Updated PHP files for login in client side.

--- LOGIN/js/index.js
+++ LOGIN/js/index.js
@@ -8,6 +8,14 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+<<<<<<< HEAD
+
+function hidePlaceHolder(event){
+    const eValue = event.currentTarget.getAttribute("placeholder");
+    event.currentTarget.setAttribute("placeholder","");
+}
+
+=======
 var placeholders = document.getElementsByTagName("input").getAttribute("placeholder");
 
 function hidePlaceHolder(event){
@@ -18,6 +26,7 @@ function restorePlaceHolder(event){
     event.currentTraget.
 }
 
+>>>>>>> 1facd3944320915824ef02887a6065e88fb46084
 
 var inputs = document.getElementsByTagName("input");
 for(var i = 0; i < inputs.length ; i++ ){
---
+++ LOGIN/php/login.php
@@ -0,0 +1,4 @@
+<?php
+$uname = $mysqli->escape_string($_POST['uname']);
+$result = $mysqli->query("select * from customers and ")
+?>
