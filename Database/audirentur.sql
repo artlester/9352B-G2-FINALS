2ee91280f0e5f45215c9a6852c097a91f5de82bd
iJeane <2168137@slu.edu.ph>
2018-05-22 21:07:14 +0800

    DB Update

--- Database/audirentur.sql
+++ Database/audirentur.sql
@@ -1,169 +1,114 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `transient_house` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `transient_house`;
+-- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: transient_house
+-- ------------------------------------------------------
+-- Server version	5.7.21
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema Audirentur
--- -----------------------------------------------------
+--
+-- Table structure for table `payment`
+--
 
--- -----------------------------------------------------
--- Schema Audirentur
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Audirentur` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `Audirentur` ;
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `reservation_no` int(11) NOT NULL,
+  `payment_date` datetime NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`payment_id`),
+  KEY `fk_payment_reservation_idx` (`reservation_no`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `Audirentur`.`customers`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Audirentur`.`customers` (
-  `customer_id` INT NOT NULL,
-  `cust_username` VARCHAR(64) NOT NULL,
-  `cust_password` VARCHAR(120) NOT NULL,
-  `cust_last_name` VARCHAR(35) NOT NULL,
-  `cust_first_name` VARCHAR(35) NOT NULL,
-  `cust_gender` ENUM('Male', 'Female') NOT NULL,
-  `cust_email` VARCHAR(254) NOT NULL,
-  `cust_contact_number` VARCHAR(15) NOT NULL,
-  `cust_avatar` LONGBLOB NULL,
-  `cust_bio` VARCHAR(125) NULL,
-  `cust_create_date` DATETIME NOT NULL,
-  `acc_status` ENUM('Pending','Accepted','Denied','Disabled') NOT NULL,
-  PRIMARY KEY (`customer_id`),
-  UNIQUE INDEX `reg_id_UNIQUE` (`customer_id` ASC),
-  UNIQUE INDEX `username_UNIQUE` (`cust_username` ASC),
-  UNIQUE INDEX `email_UNIQUE` (`cust_email` ASC))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `payment`
+--
 
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+INSERT INTO `payment` VALUES (1,2,'2018-01-27 18:12:37',1650),(2,1,'2018-02-14 04:30:00',600);
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `Audirentur`.`vendors`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Audirentur`.`vendors` (
-  `vendor_id` INT NOT NULL,
-  `ven_username` VARCHAR(64) NOT NULL,
-  `ven_password` VARCHAR(120) NOT NULL,
-  `ven_last_name` VARCHAR(35) NOT NULL,
-  `ven_first_name` VARCHAR(35) NOT NULL,
-  `ven_gender` ENUM('Male', 'Female') NOT NULL,
-  `ven_email` VARCHAR(254) NOT NULL,
-  `ven_contact_number` VARCHAR(15) NOT NULL,
-  `ven_avatar` LONGBLOB NULL,
-  `ven_bio` VARCHAR(125) NULL,
-  `ven_create_date` DATETIME NOT NULL,
-  `acc_status` ENUM('Pending','Accepted','Denied','Disabled') NOT NULL,
-  PRIMARY KEY (`vendor_id`),
-  UNIQUE INDEX `vendor_id_UNIQUE` (`vendor_id` ASC))
-ENGINE = InnoDB;
+--
+-- Table structure for table `reservation`
+--
 
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reservation` (
+  `reservation_no` int(11) NOT NULL AUTO_INCREMENT,
+  `applicant_name` varchar(45) NOT NULL,
+  `room_no` tinyint(2) NOT NULL,
+  `reserve_date` datetime NOT NULL,
+  `check_in` datetime NOT NULL,
+  `check_out` datetime NOT NULL,
+  `pay_status` enum('paid','unpaid') NOT NULL,
+  `no_of_lodgers` tinyint(2) NOT NULL,
+  `amount_payable` int(11) NOT NULL,
+  PRIMARY KEY (`reservation_no`),
+  KEY `fk_reservation_room1_idx` (`room_no`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `Audirentur`.`sounds`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Audirentur`.`sounds` (
-  `sound_id` INT NOT NULL,
-  `vendor_id` INT NOT NULL,
-  `name` VARCHAR(30) NOT NULL,
-  `description` TEXT NOT NULL,
-  `duration` INT NOT NULL,
-  `price` DOUBLE NOT NULL,
-  `rent_availability` DATETIME NOT NULL,
-  `main_image` LONGBLOB NOT NULL,
-  `alt_image1` LONGBLOB NOT NULL,
-  `alt_image2` LONGBLOB NOT NULL,
-  PRIMARY KEY (`sound_id`),
-  UNIQUE INDEX `sound_id_UNIQUE` (`sound_id` ASC),
-  INDEX `fk_sounds_vendors1_idx` (`vendor_id` ASC),
-  CONSTRAINT `fk_sounds_vendors1`
-    FOREIGN KEY (`vendor_id`)
-    REFERENCES `Audirentur`.`vendors` (`vendor_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `reservation`
+--
 
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+INSERT INTO `reservation` VALUES (1,'Jamie Lannister',13,'2018-01-27 09:42:37','2018-02-14 05:00:00','2018-02-15 04:30:00','paid',2,600),(2,'Lord Farquad',20,'2018-01-27 12:27:19','2018-02-01 12:00:00','2018-02-10 15:00:00','paid',1,1650);
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `Audirentur`.`rentals`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Audirentur`.`rentals` (
-  `rental_id` INT NOT NULL,
-  `rental_date` DATETIME NOT NULL,
-  `return_date` DATETIME NULL,
-  `customer_id` INT NOT NULL,
-  `vendor_id` INT NOT NULL,
-  `sound_id` INT NOT NULL,
-  `total_price` INT NULL,
-  `status` ENUM('Pending', 'Renting', 'Rented', 'Cancelled') NOT NULL,
-  PRIMARY KEY (`rental_id`),
-  UNIQUE INDEX `rental_id_UNIQUE` (`rental_id` ASC),
-  INDEX `fk_rentals_customers_idx` (`customer_id` ASC),
-  INDEX `fk_rentals_vendors1_idx` (`vendor_id` ASC),
-  INDEX `fk_rentals_sounds1_idx` (`sound_id` ASC),
-  CONSTRAINT `fk_rentals_customers`
-    FOREIGN KEY (`customer_id`)
-    REFERENCES `Audirentur`.`customers` (`customer_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_rentals_vendors1`
-    FOREIGN KEY (`vendor_id`)
-    REFERENCES `Audirentur`.`vendors` (`vendor_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_rentals_sounds1`
-    FOREIGN KEY (`sound_id`)
-    REFERENCES `Audirentur`.`sounds` (`sound_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+--
+-- Table structure for table `room`
+--
 
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room` (
+  `room_no` tinyint(2) NOT NULL,
+  `capacity` tinyint(2) NOT NULL,
+  `price` smallint(3) NOT NULL,
+  `room_status` enum('occupied','vacant','unavailable') NOT NULL,
+  PRIMARY KEY (`room_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `Audirentur`.`categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Audirentur`.`categories` (
-  `category_id` INT NOT NULL,
-  `category` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`category_id`))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `room`
+--
 
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES (1,5,350,'vacant'),(2,5,350,'vacant'),(3,5,375,'vacant'),(4,5,380,'vacant'),(5,5,380,'vacant'),(6,10,620,'vacant'),(7,10,620,'vacant'),(8,10,650,'vacant'),(9,10,650,'vacant'),(10,10,675,'vacant'),(11,7,500,'vacant'),(12,7,500,'vacant'),(13,4,300,'vacant'),(14,4,300,'vacant'),(15,4,300,'vacant'),(16,2,200,'vacant'),(17,2,200,'vacant'),(18,2,220,'vacant'),(19,1,150,'vacant'),(20,1,150,'vacant');
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- -----------------------------------------------------
--- Table `Audirentur`.`categorysound`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Audirentur`.`categorysound` (
-  `sound_id` INT NOT NULL,
-  `category_id` INT NOT NULL,
-  PRIMARY KEY (`sound_id`, `category_id`),
-  INDEX `fk_sounds_has_categories_categories1_idx` (`category_id` ASC),
-  INDEX `fk_sounds_has_categories_sounds1_idx` (`sound_id` ASC),
-  CONSTRAINT `fk_sounds_has_categories_sounds1`
-    FOREIGN KEY (`sound_id`)
-    REFERENCES `Audirentur`.`sounds` (`sound_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_sounds_has_categories_categories1`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `Audirentur`.`categories` (`category_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
--- -----------------------------------------------------
--- Table `Audirentur`.`admins`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Audirentur`.`admins` (
-  `admin_id` INT NOT NULL,
-  `admin_username` VARCHAR(64) NOT NULL,
-  `admin_password` VARCHAR(120) NOT NULL,
-  `admin_last_name` VARCHAR(35) NOT NULL,
-  `admin_first_name` VARCHAR(35) NOT NULL,
-  PRIMARY KEY (`admin_id`),
-  UNIQUE INDEX `admin_id_UNIQUE` (`admin_id` ASC))
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- Dump completed on 2018-05-22 18:03:33
